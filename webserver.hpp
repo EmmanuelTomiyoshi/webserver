269f524e0ccd7871a65e64b701dce183172892a5
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2024-04-07 12:10:29 -0300

    minor changes

--- webserver.hpp
+++ webserver.hpp
@@ -1,9 +1,11 @@
 #ifndef WEBSERVER_HPP
 #define WEBSERVER_HPP
 
+#include <netinet/in.h> // for htonl, htons, ntohl, ntohs
+#include <sys/socket.h>
 #include <iostream>
 #include <unistd.h>
-#include <cstdlib>
+
 #include "colors.hpp"
 
 void webtest();

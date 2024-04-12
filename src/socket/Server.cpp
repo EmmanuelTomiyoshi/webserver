3c29e88afe2218f7e21f2d799acf29ecdbd158d5
tomatte <daniel.brandao2000@gmail.com>
2024-04-12 09:53:14 -0300

    first succesfull connection to browser

--- src/socket/Server.cpp
+++ src/socket/Server.cpp
@@ -55,26 +55,32 @@ void	Server::send_message(epoll_event & event)
 {
 	if (event.events != EPOLLOUT)
 		return ;
-	char msg[] = "yeaaaaaaaaaaah!!!!!!!";
-	send(event.data.fd, msg, strlen(msg), MSG_WAITALL);
-	std::cout << "MESSAGE SENT" << std::endl;
-	epoll_ctl(_epfd, EPOLL_CTL_DEL, event.data.fd, &event);
-	close(event.data.fd);
-	std::cout << "CONNECTION CLOSED" << std::endl;
+	char msg[] = "HTTP/1.1 200\r\ncontent-type: text/html; charset=utf-8\r\n\r\nYEEEEEEEEEEEEEEAH!!!!";
+	int sent = send(event.data.fd, msg, strlen(msg) + 1, MSG_DONTWAIT);
+	if (sent != -1)
+	{
+		std::cout << "MESSAGE SENT" << std::endl;
+		close(event.data.fd);
+		epoll_ctl(_epfd, EPOLL_CTL_DEL, event.data.fd, &event);
+		std::cout << "CONNECTION CLOSED" << std::endl;
+	}
 }
 
 void Server::recv_message(epoll_event & event)
 {
 	if (event.events != EPOLLIN)
 		return ;
-	char buff[100];
-	int rsize = recv(event.data.fd, buff, 100, MSG_WAITALL);
+	char buff[5000];
+	int rsize = recv(event.data.fd, buff, 4999, MSG_DONTWAIT);
 	buff[rsize] = '\0';
 	Request http(buff);
 	std::cout << "REQUEST LINE: " << http.get_request_line() << std::endl;
 	std::cout << "METHOD: " << http.get_method() << std::endl;
-	epoll_ctl(_epfd, EPOLL_CTL_DEL, event.data.fd, &event);
-	new_epoll_event(event.data.fd, EPOLLOUT);
+	if (rsize > 0)
+	{
+		event.events = EPOLLOUT;
+		epoll_ctl(_epfd, EPOLL_CTL_MOD, event.data.fd, &event);
+	}
 	std::cout << "OUTPUT EVENT CREATED" << std::endl;
 }
 

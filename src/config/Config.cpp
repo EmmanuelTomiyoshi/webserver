94e30e092e7879127feedbc0ad289b0defc1ed70
tomatte <daniel.brandao2000@gmail.com>
2024-04-14 09:01:06 -0300

    set up base names for configuration

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -1,5 +1,24 @@
 # include <webserver.hpp>
 
+t_vec Config::_keys = {
+	"host", 
+	"server_name", 
+	"ports", 
+	"body_size",
+	"location",
+
+};
+
+t_vec Config::_route_keys = {
+	"methods",
+	"root",
+	"try_files",
+	"cgi_extensions",
+	"save_files_path",
+	"autoindex",
+	"return"
+};
+
 Config::Config(void)
 {}
 
@@ -12,4 +31,13 @@ Config::Config(Config const & rhs)
 Config::Config(std::string path)
 {
 	std::cout << "Opening: " << path << std::endl;
+
+	std::cout << "KEYS:\n";
+	for (t_vec::iterator it = _keys.begin(); it != _keys.end(); it++)
+		std::cout << *it << std::endl;
+
+	std::cout << "\nROUTE_KEYS:\n";
+	for (t_vec::iterator it = _keys.begin(); it != _keys.end(); it++)
+		std::cout << *it << std::endl;
 }
+
--- src/config/Config.hpp
+++ src/config/Config.hpp
@@ -1,9 +1,21 @@
 #ifndef CONFIG_HPP
 # define CONFIG_HPP
 # include <webserver.hpp>
+# include <map>
+# include <vector>
+
+typedef std::map<std::string, std::string> t_map;
+typedef std::vector<std::string> t_vec;
 
 class Config
 {
+	private:
+		static t_vec _keys;
+		static t_vec _route_keys;
+		t_map _rules;
+		t_map _rules_route;
+
+		void setup(void);
 	public:
 		Config(void);
 		Config(std::string path);
